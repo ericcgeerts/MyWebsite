85959265dd6e2d5c5745d10e07c01ace926fdf63
Eric Geerts <ericcgeerts@gmail.com>
2021-06-22 21:54:13 -0400

    Email Partially Working

--- assets/js/NewScripts.js
+++ assets/js/NewScripts.js
@@ -10,5 +10,35 @@ clickEmail.addEventListener("click",function(){
     console.log(document.getElementById("email").value)
     console.log(document.getElementById("subject").value)
     console.log(document.getElementById("message").value)
+    var name = String(document.getElementById("name").value)
+    //var email = document.getElementById("email").value
+    var subject = String(document.getElementById("subject").value)
+    var message = String(document.getElementById("message").value)
+    
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "text/plain");
+
+    var raw = "{\r\n    \"Name\": \"Eric Geerts\",\r\n    \"Subject\": \"EmailSubject \",\r\n    \"Body\": \"BodyTest\"\r\n}";
+    var ahh = ['{\r\n    \"Name\": \"',name,'\",'].join('');
+    var ahh1 = ['\r\n    \"Subject\": \"',subject,'\",'].join('');
+    var ahh2 = ['\r\n    \"Body\": \"',message,'\"\r\n}'].join('');
+    var combine = ahh.concat(ahh1);
+    combine = combine.concat(ahh2);
+
+    console.log(raw)
+    console.log(combine)
+
+    var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: combine,
+    redirect: 'follow'
+    };
+
+    fetch("https://tkoqr84s4c.execute-api.us-east-1.amazonaws.com/PROD/email", requestOptions)
+   .then(response => response.text())
+   .then(result => console.log(result))
+   .catch(error => console.log('error', error));
+    
     frm.reset();
 })
